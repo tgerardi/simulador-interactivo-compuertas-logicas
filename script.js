ed47d2a324f387c8cee4c30a121f45b0d48c616c
tomasgerardi07 <82547594+tomasgerardi07@users.noreply.github.com>
2023-12-24 15:58:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+function start() {
+    welcome();
+    inputs();
+}
+
+function welcome() {
+    alert("Input values for P and Q to generate the truth tables.");
+    alert("Enter 'T' for true or 'F' for false in your input.");
+}
+
+function inputs() {
+    let vp = prompt("P:");
+    let vq = prompt("Q:");
+}
+
+function AND_LogicalTable(p, q) {
+    switch(p) {
+        
+    }
+}
+
+function OR_LogicalTable(p, q) {
+    switch(p) {
+        
+    }
+}
+
+function XOR_LogicalTable(p, q) {
+    switch(p) {
+        
+    }
+}
+
+function XNOR_LogicalTable(p, q) {
+    switch(p) {
+        
+    }
+}
+
+function IFTHEN_LogicalTable(p, q) {
+    switch(p) {
+        
+    }
+}
+
+function THENIF_LogicalTable(p, q) {
+    switch(p) {
+        
+    }
+}
+
+function IFANDONLYIF_LogicalTable(p, q) {
+    switch(p) {
+        
+    }
+}
